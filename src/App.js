83d8d4d8e37cd93d05aab0e79ef0837404c63ce4
Emre Uslu <75951256+rakun256@users.noreply.github.com>
2025-01-02 15:37:48 +0300

    Navbar had been created.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Navbar from './Components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="w-full">
+      <Navbar />
     </div>
   );
 }
---
+++ src/Components/Navbar.js
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+
+const Navbar = () => {
+  const [active, setActive] = useState("home");
+
+  const navItems = [
+    { id: "home", icon: "home", label: "Home" },
+    { id: "app", icon: "smartphone", label: "App" },
+    { id: "comments", icon: "chat_bubble", label: "Comments" },
+    { id: "stats", icon: "monitoring", label: "Stats" },
+    { id: "download", icon: "download", label: "Download" },
+  ];
+
+  return (
+    <div className="fixed top-5 left-0 right-0 flex justify-center items-center z-50 ">
+      <div className="bg-backgroundLight/70 backdrop-blur-md shadow-lg rounded-full px-3 py-2 flex items-center space-x-3 md:space-x-5 transition-all duration-300 ease-in-out transform md:scale-100 scale-90">
+        <div className="flex items-center space-x-4">
+          {navItems.map((item) => (
+            <button
+              key={item.id}
+              onClick={() => setActive(item.id)}
+              className={`flex items-center space-x-1 group transition-all duration-300 ease-in-out px-2 py-1 rounded-full
+                        ${
+                          active === item.id
+                            ? "text-accentLight bg-accentDark"
+                            : "text-textLight hover:text-accentDark"
+                        }`}
+            >
+              <span className="material-symbols-rounded text-3xl transition-transform duration-300 ease-in-out flex items-center justify-center font-light">
+                {item.icon}
+              </span>
+              <span
+                className={`hidden md:flex font-light text-md overflow-hidden transition-all duration-300 ease-in-out items-center justify-center text-accentLight
+              ${
+                active === item.id
+                  ? "max-w-[100px] opacity-100"
+                  : "max-w-0 opacity-0"
+              } `}
+              >
+                {item.label}
+              </span>
+            </button>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
